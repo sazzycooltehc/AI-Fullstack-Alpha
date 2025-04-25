81fff6fa596d94b849f96e8cafa2bad072897386
sazzycooltehc <aravindsas999@gmail.com>
2025-04-25 20:00:54 +0530

    Button UI Designs

--- Frontend/Client/src/app/app.component.ts
+++ Frontend/Client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
--- Frontend/Client/src/app/login/normal-login/normal-login.component.ts
+++ Frontend/Client/src/app/login/normal-login/normal-login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-normal-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './normal-login.component.html',
   styleUrl: './normal-login.component.scss'
 })
 export class NormalLoginComponent {
+
+  login() {
+    // Implement login logic here
+    console.log('Login button clicked!');
+  }
+  forgot() {
+    // Implement forgot password logic here
+    console.log('Forgot password button clicked!');
+  }
 
 }
