5766ee807bcc2a319273d3b4a9ffea5b6eff6e1d
sazzycooltehc <aravindsas999@gmail.com>
2025-04-26 09:03:00 +0530

    IM: Backend Server definitions

---
+++ Backend/server/src/main/java/com/backend/server/controller/loginContoller.java
@@ -0,0 +1,4 @@
+package com.backend.server.controller;
+
+public class loginContoller {
+}
---
+++ Backend/server/src/main/java/com/backend/server/utils/customError.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class customError {
+    private String errorMessage;
+
+    public customError(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
