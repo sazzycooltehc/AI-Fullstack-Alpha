3dedc82b47b4343e124fefb17f4fcbec37c57150
sazzycooltehc <aravindsas999@gmail.com>
2025-04-29 22:28:28 +0530

    IM: UI Changes

--- Frontend/Client/src/app/login/normal-login/normal-login.component.ts
+++ Frontend/Client/src/app/login/normal-login/normal-login.component.ts
@@ -21,7 +21,7 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 export class NormalLoginComponent {
   loginForm: FormGroup | any;
   loginError: string = '';
-  constructor(private router: Router, private loginAPI: LoginService, private fb: FormBuilder) { 
+  constructor(private router: Router, private loginAPI: LoginService, private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -30,37 +30,42 @@ export class NormalLoginComponent {
   // Define the properties for the form component
 
   loginvalue: any;
+  hover: boolean = false;
   private http: HttpClient = inject(HttpClient);
   private baseUrl = environment.apiUrl; // Use the environment variable for the base URL
 
 
-login() {
-  // Implement login logic here
-  console.info('Login button clicked!');
-  const headers = new HttpHeaders({
-    'Content-Type': 'application/x-www-form-urlencoded',
-  });
+  login() {
+    // Implement login logic here
+    console.info('Login button clicked!');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
 
-  
-  const body = new URLSearchParams();
-  body.set('userid', this.loginForm.value.email);
-  body.set('password', this.loginForm.value.password);
-  this.http.post(environment.loginUrl, body.toString(), { headers, responseType: 'json' })
-  .subscribe({
-    next: (response: any) => {
-      localStorage.setItem('authToken', response.token); // Save token
-      localStorage.setItem('tokenTimestamp', Date.now().toString()); // Save timestamp
-      this.router.navigate(['/home']);
-     // this.router.navigate(['newsfeed']); // Navigate after login success
-    },
-    error: (err) => {
-      this.loginError = 'Incorrect password or username'; // Set error message
-    }
-  });
-}
-register() {
-  this.router.navigate(['/register']);
-  console.info('Register button clicked!');
-}
 
+    const body = new URLSearchParams();
+    body.set('userid', this.loginForm.value.email);
+    body.set('password', this.loginForm.value.password);
+    this.http.post(environment.loginUrl, body.toString(), { headers, responseType: 'json' })
+      .subscribe({
+        next: (response: any) => {
+          localStorage.setItem('authToken', response.token); // Save token
+          localStorage.setItem('tokenTimestamp', Date.now().toString()); // Save timestamp
+          this.router.navigate(['/home']);
+          // this.router.navigate(['newsfeed']); // Navigate after login success
+        },
+        error: (err) => {
+          this.loginError = 'Incorrect password or username'; // Set error message
+        }
+      });
+  }
+  register() {
+    this.router.navigate(['/register']);
+    console.info('Register button clicked!');
+  }
+
+  logoChange() {
+    this.hover = !this.hover;
+    console.log('hwllo');
+  }
 }
